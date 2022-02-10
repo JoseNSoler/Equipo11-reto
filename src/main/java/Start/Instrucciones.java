b7d1549a93931dee591109fb3bcdf7fb0e911c82
Santiago C <santiago.ci9619@gmail.com>
2022-02-10 00:15:07 -0500

    Reto terminado ;)

--- src/main/java/Start/Instrucciones.java
+++ src/main/java/Start/Instrucciones.java
@@ -1,79 +1,143 @@
 package Start;
+
 import java.util.Map;
 import java.util.Scanner;
 
 class Instrucciones {
-    private Map<String,Integer> map;
 
-    public void Instrucciones(){
+    private Map<String, Integer> map;
+    private String[] subrutina;
 
+    /**
+     * Clase Instrucciones que crea un objeto que contiene la subrutina a analizar.
+     *
+     * @param subrutina Subrutina a evaluar
+     */
+    public Instrucciones(String[] subrutina) {
+        this.subrutina = subrutina;
     }
 
-    public void mov(String x){
-        String[] Clave=x.split(",");
-        if((Clave[0].matches("[0-9]+"))){
-            map.put(Clave[1], Integer.valueOf(Clave[0]));
-        }else{
-            map.put(Clave[1],map.get(Clave[0]));
+    /**
+     * Metodo para validar que la subrutina entregada cumpla con la condicion de tener menos de 1024 instrucciones
+     */
+    public void validarSubrutina() {
+        if (this.subrutina.length > 1024) {
+            throw new IllegalArgumentException("_Error.- Maximo 1024 Instrucciones");
         }
     }
 
-    public void add (String x){
+    /**
+     * mov Funcion Ej: MOV 5,R00 || MOV R02,R42
+     * referencia = 5, R00
+     * value = 5 || R02 -- MemReferencia = R00 || R42
+     * Si value es un <int>, copiara el numero a la posicion MemReferencia
+     * Si value es un String con direccion en memoria, Copiara el valor en referencia de value
+     * en MemReferencia
+     */
+    public void mov(String referencia) {
+        String[] Clave = referencia.split(",");
 
-        String[] Clave=x.split(",");
-        int suma= (int) ((map.get(Clave[0])+map.get(Clave[1]))%(Math.pow(2,32)));
-        map.put(Clave[0],suma);
+        if ((Clave[0].matches("[0-9]+"))) {
+            // Clave [1] referencia a la posicion Ryy
+            map.put(Clave[1], Integer.valueOf(Clave[0]));
+        } else {
+            map.put(Clave[1], map.get(Clave[0]));
+        }
     }
 
-    public void dec(String x){
+    /**
+     * add funcion --EJ: ADD R02,R01
+     * referencia = R02,R01
+     * value = 5 || R02 -- MemReferencia = R00 || R42
+     * si value es un <int>, copiara el numero a la posicion MemReferencia
+     * si value es un String con direccion en memoria, Copiara el valor en referencia de value
+     */
+    public void add(String referencia) {
+        String[] Clave = referencia.split(",");
+        String MemRef1 = Clave[0];
+        String MemRef2 = Clave[1];
+
+        int suma = (int) ((map.get(MemRef1) + map.get(MemRef2)) % (Math.pow(2, 32)));
+        map.put(MemRef1, suma);
+    }
+
+
+    /**
+     * dec funcion --EJ: DEC R00
+     * MemReferencia = R00
+     * MemRef1 = R02 -- MemRef2 = R01
+     * Disminuye el valor de MemReferencia en 1. Si el valor del registro es 0, al disminuirlo_
+     * Se genera un desbordamiento y su resultado sería 232–1.
+     */
+    public void dec(String MemReferencia) {
         //obtiene el valor de la clave x
         //y lo decrementa en 1
-        int valor=map.get(x);
-        if(valor>0){
-            map.put(x,valor-1);
-        }else{
-            map.put(x, (int)(Math.pow(2,32)-1));
+        int valor = map.get(MemReferencia);
+        if (valor > 0) {
+            map.put(MemReferencia, valor - 1);
+        } else {
+            map.put(MemReferencia, (int) (Math.pow(2, 32) - 1));
         }
     }
 
-    public void inc(String x){
-        //obtiene el valor de la clave x
+
+    /**
+     * inc funcion --EJ: INC R01
+     * MemReferencia = R01
+     * Aumenta el valor de MemReferencia en 1. Si el valor del registro es 232–1, al aumentarlo_
+     * _se genera un desbordamiento obteniendo por resultado 0.
+     */
+    public void inc(String MemReferencia) {
+        //obtiene el valor de la clave referencia
         //y lo incrementa en 1
-        int valor=map.get(x);
-        if(valor==(Math.pow(2,32)-1)){
-            map.put(x,0);
-        }else{
-            map.put(x, valor+1);
+        int valor = map.get(MemReferencia);
+        if (valor == (Math.pow(2, 32) - 1)) {
+            map.put(MemReferencia, 0);
+        } else {
+            map.put(MemReferencia, valor + 1);
         }
     }
 
-    public void inv(String x){
-//Obtiene el valor de la clave x y lo convierte a bit
+    /**
+     * inv funcion --EJ: INV R01
+     * MemReferencia = R01
+     * Ejecuta una inversión bit a bit del registro MemReferencia (convierte 1 en 0 y 0 en 1).
+     */
+    public void inv(String MemReferencia) {
+        //Obtiene el valor de la clave x y lo convierte a bit
         //dentro del for lo invierte
-        String result = Integer.toBinaryString(map.get(x));
-        String cad="";
-        for (int a=0; a<result.length();a++){
-            char d=result.charAt(a);
+        String result = Integer.toBinaryString(map.get(MemReferencia));
+        String cadenaBinaria = "";
+        for (int a = 0; a < result.length(); a++) {
+            char d = result.charAt(a);
 
-            if(Character.compare(d,'0')==0){
-                cad=cad+"1";
-            }else{
-                cad=cad+"0";
+            if (Character.compare(d, '0') == 0) {
+                cadenaBinaria = cadenaBinaria + "1";
+            } else {
+                cadenaBinaria = cadenaBinaria + "0";
             }
 
         }
         //en esta parte vuelve a convertir a entero.
-        map.put(x,Integer.parseInt(cad,2));
+        map.put(MemReferencia, Integer.parseInt(cadenaBinaria, 2));
     }
 
-    public void nop(String x){
+    /**
+     * Instruccion que no realiza ninguna accion en el registro
+     */
+    public void nop() {
     }
 
-
+    /**
+     * Getter para retonar un objeto clave-valor de los registros
+     */
     public Map<String, Integer> getMap() {
         return map;
     }
 
+    /**
+     * Setter para establecer un objeto objeto clave-valor que almacene los registros
+     */
     public void setMap(Map<String, Integer> map) {
         this.map = map;
     }
--- src/main/java/Start/Main.java
+++ src/main/java/Start/Main.java
@@ -1,78 +1,82 @@
 package Start;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.stream.IntStream;
 
-public class Main
-{
+public class Main {
 
     // Se va a utilizar un map Para los registros
     public static void main(String[] args) {
-        Instrucciones intrucciones=new Instrucciones();
+        int contadorEjecuciones = 0;
         Map<String, Integer> registros = new HashMap<>();
 
+        String [] subrutina = {
+                        "MOV 5,R00",
+                        "MOV 10,R01",
+                        "JZ 7",
+                        "ADD R02,R01",
+                        "DEC R00",
+                        "JMP 3",
+                        "MOV R02,R42"
+                };
+
+        Instrucciones instrucciones = new Instrucciones(subrutina);
+
+                // Creacion de 42 registros R
         IntStream numeroRegistro = IntStream.range(0, 42);
+                // Repletar los registros con 0
         numeroRegistro.forEach(x ->
-                registros.put(String.format("R"+ (x>=10? "":"0") +"%s", x), 0));
-        intrucciones.setMap(registros);
-      /*  registros.forEach((k, v) -> System.out.printf("%s : %s%n", k, v));
+                        registros.put(String.format("R" + (x >= 10 ? "" : "0") + "%s", x), 0));
+        instrucciones.setMap(registros);
 
-        Iterator it=registros.keySet().iterator();*/
-     /*   while(it.hasNext()){
-            System.out.println(it.next());
-        }*/
-        String subrutina[]={
-                "MOV 5,R00",
-                "MOV 10,R01",
-                "JZ 7",
-                "ADD R02,R01",
-                "DEC R00",
-                "JMP 3",
-                "MOV R02,R42"
-        } ;
 
-        for (int i=0;i<subrutina.length;i++){
-            String[] algo=subrutina[i].split("\\s+");
-            String accion=algo[0];
-            switch (accion){
-                case "MOV":
-                    intrucciones.mov(algo[1]);
-                    break;
-                case "JZ":
-                    int valo=intrucciones.getMap().get("R00");
-                    if(valo==0){
-                        i=Integer.valueOf(algo[1])-2;
-                    }
-                    break;
-                case "ADD":
-                    intrucciones.add(algo[1]);
-                    break;
-                case "DEC":
-                    intrucciones.dec(algo[1]);
-                    break;
-                case "INC":
-                    intrucciones.inc(algo[1]);
-                    break;
-                case "INV":
-                    break;
-                case "JMP":
-                    i=Integer.valueOf(algo[1])-2;
-                    break;
-                case "NOM":
-                    break;
-
+        instrucciones.validarSubrutina();
+        // Iteracion sobre todas las instrucciones en base a posSubRutina
+        for (int posSubrutina = 0; posSubrutina < subrutina.length; posSubrutina++) {
+            if (contadorEjecuciones++ <= 50000) {
+                // rutinaInstruccion = MOV 5,R00
+                // rutinaInstruccion[0] = MOV -- rutinaInstruccion[1] = 5, R00
+                String[] rutinaInstruccion = subrutina[posSubrutina].split("\\s+");
+                // nombreInstruccion = MOV
+                String nombreInstruccion = rutinaInstruccion[0];
+                switch (nombreInstruccion) {
+                    case "MOV":
+                        instrucciones.mov(rutinaInstruccion[1]);
+                        break;
+                    case "JZ":        // Dependiendo de si el valor en R00 en 0// Dependiendo de si el valor en R00 en 0/ Dependiendo de si el valor en R00 en 0
+                       int instruccionN = instrucciones.getMap().get("R00");
+                        // Dependiendo de si el valor en R00 en 0
+                        if (instruccionN == 0) {
+                            // Salta la iteracion hasta la instruccion valor R00
+                            posSubrutina = Integer.parseInt(rutinaInstruccion[1]) - 2;
+                        }
+                        break;
+                    case "ADD":
+                        instrucciones.add(rutinaInstruccion[1]);
+                        break;
+                    case "DEC":
+                        instrucciones.dec(rutinaInstruccion[1]);
+                        break;
+                    case "INC":
+                        instrucciones.inc(rutinaInstruccion[1]);
+                        break;
+                    case "INV":
+                        instrucciones.inv(rutinaInstruccion[1]);
+                        break;
+                    case "JMP":
+                        // Salto incondicional a N instruccion
+                        posSubrutina = Integer.parseInt(rutinaInstruccion[1]) - 2;
+                        break;
+                    case "NOP":
+                        instrucciones.nop();
+                        break;
+                }
+            } else {
+                throw new IllegalArgumentException("_Error.- Maximo de iteraciones alcanzados");
+                //System.out.println("_Error.- Maximo de iteraciones alcanzados");
             }
-
-
-        }System.out.println(intrucciones.getMap().get("R42"));
-
-        //registros.forEach(System.out::println());
-
-        /* for(int i = 42; i <=0; i--){
-            registros.put()
-        } */
+        }
+        System.out.println(instrucciones.getMap().get("R42"));
     }
-
 }
